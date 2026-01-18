7aad822a99aa7349f75d4148ceecc92dda071d88
Travers Ching <traversc@gmail.com>
2026-01-18 06:10:12 +0000

    version 0.18.0

--- inst/doc/vignette.R
+++ inst/doc/vignette.R
@@ -2,15 +2,15 @@
 IS_GITHUB <- Sys.getenv("IS_GITHUB") != ""
 
 ## ----results='asis', echo=FALSE, eval=IS_GITHUB-------------------------------
-#  cat('
-#  [![R-CMD-check](https://github.com/traversc/stringfish/workflows/R-CMD-check/badge.svg)](https://github.com/traversc/stringfish/actions)
-#  [![CRAN-Status-Badge](https://www.r-pkg.org/badges/version/stringfish)](https://cran.r-project.org/package=stringfish)
-#  [![CRAN-Downloads-Badge](https://cranlogs.r-pkg.org/badges/stringfish)](https://cran.r-project.org/package=stringfish)
-#  [![CRAN-Downloads-Total-Badge](https://cranlogs.r-pkg.org/badges/grand-total/stringfish)](https://cran.r-project.org/package=stringfish)
-#  ')
+# cat('
+# [![R-CMD-check](https://github.com/traversc/stringfish/workflows/R-CMD-check/badge.svg)](https://github.com/traversc/stringfish/actions)
+# [![CRAN-Status-Badge](https://www.r-pkg.org/badges/version/stringfish)](https://cran.r-project.org/package=stringfish)
+# [![CRAN-Downloads-Badge](https://cranlogs.r-pkg.org/badges/stringfish)](https://cran.r-project.org/package=stringfish)
+# [![CRAN-Downloads-Total-Badge](https://cranlogs.r-pkg.org/badges/grand-total/stringfish)](https://cran.r-project.org/package=stringfish)
+# ')
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  install.packages("stringfish", type="source", configure.args="--with-simd=AVX2")
+# install.packages("stringfish", type="source", configure.args="--with-simd=AVX2")
 
 ## ----echo=FALSE, results='asis'-----------------------------------------------
 if(IS_GITHUB) {
@@ -20,6 +20,6 @@ if(IS_GITHUB) {
 }
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  sf_alternate_case("hello world")
-#  [1] "hElLo wOrLd"
+# sf_alternate_case("hello world")
+# [1] "hElLo wOrLd"
 
